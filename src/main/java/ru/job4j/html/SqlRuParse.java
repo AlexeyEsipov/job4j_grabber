de4e75bd0102325efe53ac4cdd2244f34938a2ab
AlexeyEsipov <a_esipov@list.ru>
2021-01-04 19:09:24 +0300

    2.1. Преобразование даты [#379849]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,8 +4,28 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class SqlRuParse {
+
+    public static LocalDateTime dateConvert(String s) {
+        LocalDate date = LocalDate.now();
+        LocalTime lt = LocalTime.parse(s.substring(s.length() - 5));
+        if (s.startsWith("вчера")) {
+            return date.minusDays(1).atTime(lt);
+        } else if (s.startsWith("сегодня")) {
+            return date.atTime(lt);
+        } else {
+            StringBuilder sb = new StringBuilder(s);
+            return LocalDateTime.parse(sb.insert(s.length() - 10, "."),
+                    DateTimeFormatter.ofPattern("d MMM uu, HH:mm", new Locale("ru", "RU")));
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Element forumTable = doc.select("table").get(2);
@@ -15,7 +35,10 @@ public class SqlRuParse {
             Elements cols = row.select("td");
             System.out.println(cols.get(1).child(0).attr("href"));
             System.out.println(cols.get(1).child(0).text());
+            String s = cols.get(5).text();
             System.out.println(cols.get(5).text());
+            LocalDateTime dateJava = dateConvert(s);
+            System.out.println(dateJava);
         }
     }
 }
