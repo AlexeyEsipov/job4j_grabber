40995158a024396aa7c2a6c27c923ab9b420dafb
AlexeyEsipov <a_esipov@list.ru>
2021-03-29 00:36:34 +0300

    1. Отчеты. [#379966]

---
+++ src/test/java/ru/job4j/design/srp/HtmlReportEngineTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.design.srp;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+import java.util.Calendar;
+
+public class HtmlReportEngineTest {
+    @Test
+    public void whenHTMLGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        store.add(worker);
+        Report engine = new HtmlReportEngine(store);
+        StringBuilder expect = new StringBuilder()
+                .append("<!DOCTYPE html>")
+                .append(System.lineSeparator())
+                .append("<html>")
+                .append(System.lineSeparator())
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(";")
+                .append(worker.getHired()).append(";")
+                .append(worker.getFired()).append(";")
+                .append(worker.getSalary()).append(";")
+                .append(System.lineSeparator())
+                .append("</html>");
+        assertThat(engine.generate(em -> true), is(expect.toString()));
+    }
+}
